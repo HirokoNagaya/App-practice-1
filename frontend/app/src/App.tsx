a507a037d00078843621a257e397e35f705ddef7
HirokoNagaya <hiroko.nagaya@divx.co.jp>
2023-12-14 23:04:44 +0900

    バックエンドをリスト表示

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Postの型定義
+type Post = {
+  id: number;
+  title: string;
+  content: string;
+  // 他の必要なプロパティをここに追加
+};
+
+// Stateの型定義
+type State = {
+  posts: Post[];
+};
+
+
+// Axios インスタンスの作成
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3001',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    },
+    responseType: 'json'
+});
+
+class App extends React.Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    // 作成したインスタンスを使用
+    axiosInstance.get('/posts')
+    .then(response => {
+        console.log(response.data);
+        this.setState({ posts: response.data });
+    })
+    .catch(() => {
+        console.log('通信に失敗しました。');
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My React App!</h1>
+        <ul>
+          {this.state.posts.map((post: Post) => (
+            <li key={post.id}>{post.title}{post.content}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
